8f42d7f03dd8f417ff863328468dd6d3e38f1708
Bruno Kennedy <brunokennedy16@gmail.com>
2020-05-25 23:38:52 -0300

    Create global styles

---
+++ src/styles/global/globalStyles.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html, body, #root {
+    width: 100%;
+    height: 100%;
+    background-color: #f0f0f5;
+  }
+`;
