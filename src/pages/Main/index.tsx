7be78570eaf816a752e5a25ff739a4764d2164a2
Bruno Kennedy <brunokennedy16@gmail.com>
2020-05-27 13:33:24 -0300

    Edit main page: switch icon for text 'details'

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import NavbarApp from '../../components/NavbarApp';
-
-// import { Container } from './styles';
+import React, { useState, useEffect } from 'react';
+import { productsData } from '../../services/api';
+import { ProductCard } from './styles';
+import Product from '../../domains/product.interface';
 
 const Main: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    setProducts(productsData);
+  }, []);
+
   return (
-    <NavbarApp />
+    <div className="container-fluid">
+      <div className="row">
+        {products.map(product => (
+          <div
+            className="col-xl-4 col-lg-6 col-md-6 col-sm-12"
+            key={product.id}
+          >
+          <ProductCard>
+            <img src={product.photo} alt={product.name}/>
+            <div>
+              <strong>{product.name}</strong>
+              <p>{product.description}</p>
+              <div>
+                <span>R${product.price}</span>
+                <p>Detalhes</p>
+                {/* <FiShoppingCart size={24} color='gold'/> */}
+              </div>
+            </div>
+          </ProductCard>
+        </div>
+        ))}
+       </div>
+    </div>
   );
 }
 
