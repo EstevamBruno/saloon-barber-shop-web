d8d474e4eb3369c4c9217371e276873950d9bf9b
Bruno Kennedy <brunokennedy16@gmail.com>
2020-05-27 13:35:12 -0300

    Adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyles } from './styles/global/globalStyles';
 import Routes from './routes';
+import NavbarApp from './components/NavbarApp';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
+      <NavbarApp />
       <Routes />
     </BrowserRouter>
   );
