22bc5ad49246d68bfba8b84a4592794af9b6b4ad
Bruno Kennedy <brunokennedy16@gmail.com>
2020-05-27 13:31:17 -0300

    Create mock

---
+++ src/services/api.ts
@@ -0,0 +1,39 @@
+import Product from "../domains/product.interface";
+
+export const productsData: Product[] = [
+  {
+    id: 'xtoplekjdmdhsaak',
+    name: 'Tesouras De Ouro',
+    photo: 'https://d26lpennugtm8s.cloudfront.net/stores/818/083/products/tesouralasertesouranavalhatesouradesfiadeirafanaerimportada6polegadaspretacabelobarbabigodecortecabelocabeleireirobarbeiroprofissionaldouradodourada1-b6b0d82c3766cc3c4b15302821348705-1024-1024.jpg',
+    description: 'Tesouras para cortar cabelo feitas a Ouro.',
+    price: '243,79',
+  },
+  {
+    id: 'eorjaksndikasd',
+    name: 'Tesouras De Ouro',
+    photo: 'https://d26lpennugtm8s.cloudfront.net/stores/818/083/products/tesouralasertesouranavalhatesouradesfiadeirafanaerimportada6polegadaspretacabelobarbabigodecortecabelocabeleireirobarbeiroprofissionaldouradodourada1-b6b0d82c3766cc3c4b15302821348705-1024-1024.jpg',
+    description: 'Tesouras para cortar cabelo feitas a Ouro.',
+    price: '243,79',
+  },
+  {
+    id: 'asdhk7ofghjgfkyu',
+    name: 'Tesouras De Ouro',
+    photo: 'https://d26lpennugtm8s.cloudfront.net/stores/818/083/products/tesouralasertesouranavalhatesouradesfiadeirafanaerimportada6polegadaspretacabelobarbabigodecortecabelocabeleireirobarbeiroprofissionaldouradodourada1-b6b0d82c3766cc3c4b15302821348705-1024-1024.jpg',
+    description: 'Tesouras para cortar cabelo feitas a Ouro.',
+    price: '243,79',
+  },
+  {
+    id: 'gdfjuyidfguoi7ui',
+    name: 'Tesouras De Ouro',
+    photo: 'https://d26lpennugtm8s.cloudfront.net/stores/818/083/products/tesouralasertesouranavalhatesouradesfiadeirafanaerimportada6polegadaspretacabelobarbabigodecortecabelocabeleireirobarbeiroprofissionaldouradodourada1-b6b0d82c3766cc3c4b15302821348705-1024-1024.jpg',
+    description: 'Tesouras para cortar cabelo feitas a Ouro.',
+    price: '243,79',
+  },
+  {
+    id: 'asgfhjyu6isdefg',
+    name: 'Tesouras De Ouro',
+    photo: 'https://d26lpennugtm8s.cloudfront.net/stores/818/083/products/tesouralasertesouranavalhatesouradesfiadeirafanaerimportada6polegadaspretacabelobarbabigodecortecabelocabeleireirobarbeiroprofissionaldouradodourada1-b6b0d82c3766cc3c4b15302821348705-1024-1024.jpg',
+    description: 'Tesouras para cortar cabelo feitas a Ouro.',
+    price: '243,79',
+  }
+];
