05fd5d96fcc8cb39221917d7d482598598e3b84b
Bruno Kennedy <brunokennedy16@gmail.com>
2020-05-30 16:13:35 -0300

    Styling application container

---
+++ src/styles/AppContainer/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const AppContent = styled.div`
+  height: 100%;
+`;
